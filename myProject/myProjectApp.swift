d8ce259474594f2710ebd669c7bfddd0c143a45f
adhatesanyukta <sanyukta.adhate@bookmyshow.com>
2025-12-29 13:57:25 +0530

    Initial Commit

---
+++ myProject/myProjectApp.swift
@@ -0,0 +1,32 @@
+//
+//  myProjectApp.swift
+//  myProject
+//
+//  Created by Sanyukta Adhate on 29/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct myProjectApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
